89131f0213bd68547161ef5416ebaa77747b3591
PeCMay2105 <pedrocmaia2105@gmail.com>
2023-02-23 12:26:13 -0300

    novos projetos

--- cursoDeArrays.js
+++ cursoDeArrays.js
@@ -1,24 +1,15 @@
-const notas = [10,6,8,5.5,10]
-notas.pop(10)
-
-//console.log(notas)
-
-let media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length
-//console.log(media)
-
-let mediaAuto = (notas.reduce(calculo,0))/notas.length
-
-function calculo(valor1,valor2){
-return valor1 + valor2
+const alunos =['Marcos','Felipe','Joao','César','Bruna','Cleide','Luiza']
+const notas = [8,10,8,7,5,9,8]
+const AlunosENotas= [ alunos, notas]
+function verificaAluno(aluno){
+    if(AlunosENotas[0].includes(aluno)){
+        let indice = AlunosENotas[0].indexOf(aluno)
+        let nota = AlunosENotas[1][indice]
+        console.log(`A média do aluno ${aluno} é ${nota}`)
+    }
+    else{
+        console.log('este aluno não está cadastrado')
+    }
 }
-//onsole.log(mediaAuto)
-
-
-
-
-const sala1 = ['marcos','cesar','clara','maria']
-const sala2 = ['carlos','felipe','pedro','artur']
-let sala3 = sala1.concat(sala2)
-console.log(sala1)
-console.log(sala2)
-console.log(sala3)
+verificaAluno('Felipe')
+verificaAluno('Marcos')
